837e318f4d9a6c2f02f7766d83e248a114fdf527
ArtSkellige <ArtSkelligeBravoZulu141@protonmail.com>
2026-01-31 07:36:13 +0300

    Initial commit: Add project structure

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Financial Marketing Form Letter Generator
+Generates personalized investment marketing letters using string interpolation and formatting.
+*/
+
+string customerName = "Ms. Barros";
+
+String currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},\n");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += $"{currentReturn:P2}".PadLeft(9);
+comparisonMessage += $"{currentProfit:C}";
+comparisonMessage += "\n";
+
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += $"{newReturn:P2}".PadLeft(9);
+comparisonMessage += $"{newProfit:C}";
+
+Console.WriteLine(comparisonMessage);
